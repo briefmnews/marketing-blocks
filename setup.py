bc2ef7a5ffed670791e22bc688030f6433db8b67
Thomas C <thomas.carbillet@gmail.com>
2019-07-23 11:12:45 +0200

    chore(): add trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+
 setup(
     name="marketing-blocks",
     version="0.1.3",
@@ -9,7 +10,18 @@ setup(
     author_email="tech@brief.me",
     license=None,
     packages=["marketing_blocks", "marketing_blocks.migrations"],
-    python_requires="==2.7.*",
+    python_requires=">=2.7",
     install_requires=["Django>=1.11.21", "django-model-utils>=3.1.2"],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
     zip_safe=False,
 )
