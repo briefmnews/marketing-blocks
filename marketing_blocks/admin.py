e4d8785fc62a1c14aadb42f9871049b48e73e46f
Thomas <thomas.carbillet@gmail.com>
2020-09-16 15:12:57 +0200

    feat: set app_label field as a ChoiceField in admin

--- marketing_blocks/admin.py
+++ marketing_blocks/admin.py
@@ -1,14 +1,26 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
+from django.conf import settings
 from django.contrib import admin
 
 from .models import MarketingBlock
 
+MARKETING_BLOCKS_LABEL_CHOICES = getattr(settings, "MARKETING_BLOCKS_LABEL_CHOICES", "")
+
+
+class MarketingBlocksAdminForm(forms.ModelForm):
+    label = forms.ChoiceField(choices=MARKETING_BLOCKS_LABEL_CHOICES, required=False)
+
+    class Meta:
+        model = MarketingBlock
+        fields = "__all__"
+
 
 class MarketingBlocksAdmin(admin.ModelAdmin):
-    list_display = ("title", "position", "active", "modified")
-    list_filter = ("position",)
+    list_display = ("label", "title", "position", "active", "modified")
+    list_filter = ("position", "label")
+    form = MarketingBlocksAdminForm
 
 
 admin.site.register(MarketingBlock, MarketingBlocksAdmin)
