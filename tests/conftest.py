0e189679e712d840a205bf29178beb68289d2244
Thomas <thomas.carbillet@gmail.com>
2020-09-16 16:05:13 +0200

    test: fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,10 @@ from .factories import MarketingBlocksFactory
 @pytest.fixture
 def active_header_block():
     return MarketingBlocksFactory(
-        title="Active Header", content_mailchimp="I'm an active header", active=True
+        title="Active Header",
+        content_mailchimp="I'm an active header",
+        content_sendgrid="I'm a header too",
+        active=True,
     )
 
 
@@ -15,6 +18,7 @@ def active_footer_block():
     return MarketingBlocksFactory(
         title="Active Footer",
         content_mailchimp="I'm an active footer",
+        content_sendgrid="I'm an active footer too",
         active=True,
         position="footer",
     )
--- tests/test_managers.py
+++ tests/test_managers.py
@@ -9,7 +9,9 @@ class TestMarketingBlockManagers(object):
     def test_get_active_block_for_position(self, active_header_block):
         """Return the last active block for a specific position"""
         # WHEN
-        header = MarketingBlock.objects.get_active_block_for_position("header")
+        header = MarketingBlock.objects.get_active_block_for_position_and_label(
+            "header"
+        )
 
         # THEN
         assert header.title == "Active Header"
@@ -21,8 +23,8 @@ class TestMarketingBlockManagers(object):
     ):
         """Returns all the active block contents as a dictionary."""
         # WHEN
-        dict_blocks = MarketingBlock.objects.get_block_contents_by_position(
-            backend="mailchimp"
+        dict_blocks = MarketingBlock.objects.get_block_contents_by_position_for_label(
+            backend="mailchimp", label=""
         )
 
         # THEN
@@ -37,8 +39,8 @@ class TestMarketingBlockManagers(object):
         assert not hasattr(active_header_block, "content_stromboli")
 
         # WHEN
-        dict_blocks = MarketingBlock.objects.get_block_contents_by_position(
-            backend="stromboli"
+        dict_blocks = MarketingBlock.objects.get_block_contents_by_position_for_label(
+            backend="stromboli", label=""
         )
 
         # THEN
