1a0317bedf47a8357c10640b881c797348cdfa5d
Thomas <thomas.carbillet@gmail.com>
2021-03-17 15:52:51 +0100

    chore: release version 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="marketing-blocks",
-    version="1.3.0",
+    version="1.3.1",
     description="Handle marketing blocks from back office",
     url="https://github.com/briefmnews/marketing-blocks",
     author="Brief.me",
